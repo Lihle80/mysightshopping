382aebbb32c8b6daa8d8ee9975716e6b37946bc0
mila0523 <103040988+mila0523@users.noreply.github.com>
2022-05-11 17:15:02 +0200

    Add files via upload

---
+++ mysight.co.za/js/main.js
@@ -0,0 +1,10 @@
+//nav bar operation
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+
+  }
