117662553b5496b334806172ccdcbaa07ab1b571
mila0523 <103040988+mila0523@users.noreply.github.com>
2022-05-12 00:39:04 +0200

    Add files via upload

--- mysight.co.za/js/main.js
+++ mysight.co.za/js/main.js
@@ -8,3 +8,32 @@ function myFunction() {
     }
 
   }
+
+function scroll(){
+  $(window).scroll(function () {
+    var height = $(window).height();
+    var scroll = $(window).scrollTop();
+    if (scroll) {
+      $(".header-hide").addClass("scroll-header");
+    } else {
+      $(".header-hide").removeClass("scroll-header");
+    }
+
+  });
+
+ // Back to top button
+ $(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+}
+   
+
+
