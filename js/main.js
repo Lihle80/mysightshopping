abc2f95cbd0433dc7accdc2db266e4aa96cbbcb2
u21589276 <102968933+u21589276@users.noreply.github.com>
2022-05-12 17:41:54 +0200

    mysight

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//nav bar operation
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+
+  }
+
+function scroll(){
+  $(window).scroll(function () {
+    var height = $(window).height();
+    var scroll = $(window).scrollTop();
+    if (scroll) {
+      $(".header-hide").addClass("scroll-header");
+    } else {
+      $(".header-hide").removeClass("scroll-header");
+    }
+
+  });
+
+ // Back to top button
+ $(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+}
+   
+
+
---
+++ js/productdata.js
@@ -0,0 +1,103 @@
+let products = [
+    {
+        Name: "hoodie",  
+        Price: 200,
+        Sku: "", 
+        color: "",
+        imgs: "",
+        size: "",
+        StockQuantity: 0
+    
+    },
+    {
+        Name: "Golfer",  
+        Price: 200,
+        Sku: "", 
+        color: "",
+        imgs: "",
+        size: "",
+        StockQuantity: 0
+    
+    },
+    {
+        Name: "t-shirt",  
+        Price: 200,
+        Sku: "", 
+        color: "",
+        imgs: "",
+        size: "",
+        StockQuantity: 0
+    
+    },
+    {
+        Name: "cap",  
+        Price: 200,
+        Sku: "", 
+        color: "",
+        imgs: "",
+        size: "",
+        StockQuantity: 0
+    
+    },    {
+        Name: "hoodie",  
+        Price: 200,
+        Sku: "", 
+        color: "",
+        imgs: "",
+        size: "",
+        StockQuantity: 0
+    
+    },
+    {
+        Name: "Golfer",  
+        Price: 200,
+        Sku: "", 
+        color: "",
+        imgs: "",
+        size: "",
+        StockQuantity: 0
+    
+    },
+    {
+        Name: "t-shirt",  
+        Price: 200,
+        Sku: "", 
+        color: "",
+        imgs: "",
+        size: "",
+        StockQuantity: 0
+    
+    }
+]
+
+function onShopPageLoad()
+{
+    //log products array to console
+    console.log(products);
+
+    
+  var table = document.getElementById("tbody");
+  for( let i = 0; i<products.length; i++)
+  {
+    var prodName = products[i].Name;
+    var prodPrice = products[i].Price;
+
+    //add  cards
+    var rowcards = document.getElementById("pdCards");
+
+     rowcards.innerHTML += `<div class="col-sm-3">
+        <div class="card">
+        <div class="card-header" id="productName">${prodName}</div>
+        <div class="card-body" id="b1"></div> 
+        <div class="card-footer" id="f1"><h4>R${prodPrice}</h4></div>
+        </div>
+        </div>`
+ 
+      
+    document.getElementById("productName").innerHTML = prodName;
+  
+  }
+
+      
+
+}
