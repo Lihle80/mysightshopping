c2b5b692724d2c79c6872f96c67dce47e5694f70
mila0523 <103040988+mila0523@users.noreply.github.com>
2022-07-06 21:17:21 +0200

    Add files via upload

--- js/productdata.js
+++ js/productdata.js
@@ -14,7 +14,7 @@ const url= `${base2}&sheet=${sheetName2}&tq=${query2}`;
 
 //DOM function lisatener
 const client = []
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener('DOMContentLoaded', init, populateDetailsModal)
  
 /*INT FUNCTION TO PROCCESS ***This function fetches data from the google sheet detailed above
 after the data is fethced in the function, it is then changed from an unstructures array 
@@ -39,7 +39,8 @@ function init(){
           var productName =	prodArr[i].c[1].v;
           var prdDescription =	prodArr[i].c[2].v;
           var prdPrice = prodArr[i].c[3].v;
-          var imgUrl = prodArr[i].c[4].v;
+          var stockStatus = prodArr[i].c[4].v;
+          var imgUrl = prodArr[i].c[5].v;
 
 
           //contruction a proper structured array of the data
@@ -47,12 +48,18 @@ function init(){
                 "Prd_Code":	prd_Code,
                 "Name": productName,	
                 "Description": prdDescription,	
-                "Price": prdPrice,	
+                "Price": prdPrice,
+                "StockStatus": stockStatus,
                 "img_url": imgUrl
             });
-          
-            //google drive links
-            //https://drive.google.com/uc?export=view&id=0B6wwyazyzml-OGQ3VUo0Z2thdmc
+    
+            var statustColor;
+             if(stockStatus == "Out of stock"){
+              statustColor = "redofs";
+            }
+            else{
+              statustColor = "greenofs";
+            }
 
             try{
     				prodCardinner.innerHTML += `
@@ -62,14 +69,15 @@ function init(){
     						<h4>${productName}</h4>
     						</div>
 
-                <div class="card-body">
-    						<img src="${imgUrl}" class="card-img-top" alt="${productName} img" height="100%">
+                <div class="card-body text-center">
+    						<img src="${imgUrl}" alt="${productName} img" width="100%" height="90%">
     		        </div>
 
                 <div class="card-footer">
     							<h4 class="card-title">R ${prdPrice}.00</h4>
     							<p class="card-text"></p>
-    							<button type="button" id="prd_Code" class="btn btn-warning" data-toggle="modal" data-target="#productCenter">View Product</button>
+    							<button type="button" id="${prd_Code}" onclick="populateDetailsModal(this.id)" class="btn btn-warning" data-toggle="modal" data-target="#productCenter">View Product</button>
+                  &nbsp;<span class="${statustColor}" id="stkStatus">${stockStatus}</span>
     						</div>
     					</div>
     				</div>`;
@@ -77,7 +85,60 @@ function init(){
             catch{
               
             }
+
+
         }
         console.log(nw);
+
+
     })        
+}
+
+//global variables for the modal
+var modinnerFoot, modinnerBody, modinnerTitle;
+modinnerTitle = document.getElementById("modinnerTitle");
+modinnerBody = document.getElementById("modinnerBody");
+modinnerFoot =document.getElementById("modinnerFoot");
+
+function populateDetailsModal(clickedId){
+
+  //retrive the correct id for thew product
+  modinnerTitle.innerHTML = clickedId;
+
+  fetch(url)
+    .then(res => res.text())
+    .then(rep => {
+        //Remove additional text and extract only JSON:
+        const jsonData2 = JSON.parse(rep.substring(47).slice(0, -2));
+        //rows of the data retrieved
+        const prodArr = jsonData2.table.rows;
+        //=============LOG TO CONSOLE========================//
+        //console.log(jsonData2);
+        //console.log(prodArr);
+
+        let nw = [ ];
+    
+        for (let i = 0; i < prodArr.length; i++) {
+      
+          var prd_Code = prodArr[i].c[0].v;
+          var productName = prodArr[i].c[1].v;
+          var prdDescription = prodArr[i].c[2].v;
+          var prdPrice = prodArr[i].c[3].v;
+          var imgUrl = prodArr[i].c[4].v;
+
+
+          //contruction a proper structured array of the data
+          nw.push({
+                "Prd_Code": prd_Code,
+                "Name": productName,  
+                "Description": prdDescription,  
+                "Price": prdPrice,  
+                "img_url": imgUrl
+            });
+        }
+
+        /*if(){
+
+        }*/
+    });        
 }
