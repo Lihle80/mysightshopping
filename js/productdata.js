3589720bf1530dcfbcacb2793cdf1aec52a86a1c
mila0523 <103040988+mila0523@users.noreply.github.com>
2022-07-07 02:03:51 +0200

    Add files via upload

--- js/productdata.js
+++ js/productdata.js
@@ -63,14 +63,14 @@ function init(){
 
             try{
     				prodCardinner.innerHTML += `
-    				<div class="col-sm">
+    				<div class="col-md">
     					<div class="card chover" style="width: 35rem; height: 55rem;">
     						<div class="card-header bg-dark text-light">
     						<h4>${productName}</h4>
     						</div>
 
                 <div class="card-body text-center">
-    						<img src="${imgUrl}" alt="${productName} img" width="100%" height="90%">
+    						<img src="${imgUrl}" alt="${productName} img" width="100%" height="100%">
     		        </div>
 
                 <div class="card-footer">
@@ -95,15 +95,17 @@ function init(){
 }
 
 //global variables for the modal
-var modinnerFoot, modinnerBody, modinnerTitle;
+var modinnerFoot, modinnerBody, modinnerTitle, prodleft, right;
 modinnerTitle = document.getElementById("modinnerTitle");
 modinnerBody = document.getElementById("modinnerBody");
 modinnerFoot =document.getElementById("modinnerFoot");
+prodleft = document.getElementById("prodleft");
+prodright = document.getElementById("prodright");
 
 function populateDetailsModal(clickedId){
 
   //retrive the correct id for thew product
-  modinnerTitle.innerHTML = clickedId;
+  modinnerTitle.innerHTML = "Item Code: " + clickedId;
 
   fetch(url)
     .then(res => res.text())
@@ -135,10 +137,65 @@ function populateDetailsModal(clickedId){
                 "Price": prdPrice,  
                 "img_url": imgUrl
             });
+
+          if(clickedId == prd_Code){
+            prodright.innerHTML = `<h1>All the images and related gallery for this product will go here.</h1>`;
+
+            prodleft.innerHTML = `
+            <h4>${productName}</h4>
+            <hr>
+            <h5>Description</h5>
+            <p>${prdDescription}</p>
+            <hr>
+            <div id="frmsize">
+              <h5><b>Select size</b></h5>
+              <form class="size">
+                <label>Small</label>
+                <input type="radio" name="sizeRad"><br>
+                <label>Medium</label>
+                <input type="radio" name="sizeRad"><br>
+                <label>Large</label>
+                <input type="radio" name="sizeRad"><br>
+                <label>XLarge</label>
+                <input type="radio" name="sizeRad"><br>
+                <label>2XLarge</label>
+                <input type="radio" name="sizeRad">
+              <from>
+              <hr>
+            </div>
+
+            <div id="frmcolor">
+              <h5><b>Select a color</b></h5>
+              <form class="colorfrm">
+                <label>Red</label>
+                <input type="radio" name="capColor"><br>
+                <label>Blue</label>
+                <input type="radio" name="capColor"><br>
+              <from>
+              <hr>
+             </div>
+            <h4>R ${prdPrice}.00</h4>`;
+
+            if(productName.includes("Cap") == true){
+              document.getElementById("frmsize").style.display = "none";
+              document.getElementById("frmcolor").style.display = "none";
+            }
+            else if(productName.includes("Cap Red & Blue") == true){
+              document.getElementById("frmsize").style.display = "none";
+              document.getElementById("frmcolor").style.display = "block";
+            }
+            else if(productName.includes("Beanie") == true){
+              document.getElementById("frmsize").style.display = "none";
+              document.getElementById("frmcolor").style.display = "none";
+            }
+            else{
+              document.getElementById("frmsize").style.display = "block";
+              document.getElementById("frmcolor").style.display = "none";
+            }
+
+          }
+
         }
 
-        /*if(){
-
-        }*/
     });        
 }
